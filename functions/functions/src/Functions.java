9ce10940acca664d1760294a6ecb70d08c9cd86b
radkoff26 <slaradko@gmail.com>
2020-11-03 11:41:19 +0300

    added Pythagoras' theorem

--- functions/functions/src/Functions.java
+++ functions/functions/src/Functions.java
@@ -1,4 +1,5 @@
 public class Functions {
+    //Sqrt function that displays output with root
     public static String sqrt_string(double a) {
         int start =(int) Math.ceil((Math.ceil(Math.sqrt(a))) / 2);
         int mid = 1;
@@ -17,5 +18,21 @@ public class Functions {
         } else {
             return mid + "√" + a;
         }
+    }
+    //Solution of the theorem of Pythagoras that finds hypotenuse
+    public static String theorem_of_Pythagoras_finding_hypotenuse(double a, double b) {
+        double h;
+        h = a * a + b * b;
+        return sqrt_string(h);
+    }
+    //Solution of the theorem of Pythagoras that finds leg
+    public static String theorem_of_Pythagoras_finding_leg(double h, double a) {
+        //Throwing error if hypotenuse less than or equal to leg
+        if (h <= a) {
+            return "Negative output!";
+        }
+        double b;
+        b = h * h - a * a;
+        return sqrt_string(b);
     }
 }
